f12d64c669dba54d5ff26d0dedd82c8194b86a2c
Nathan Godard <nathan.godard1@ucalgary.ca>
2018-10-25 21:15:17 -0600

    Initial Commit

---
+++ ENSF480Lab6_ExC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ENSF480Lab6_ExC
+//
+//  Created by Nathan Godard on 2018-10-25.
+//  Copyright © 2018 Nathan Godard. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
